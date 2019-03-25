aff964a958a54e364529c3b34b4d05ecc0f4ff84
MariusKin <marius.kinstaitis@gmail.com>
2019-03-25 20:12:26 +0200

    nex commit

--- src/eu/kinstaitis/registration/test/Testas.java
+++ src/eu/kinstaitis/registration/test/Testas.java
@@ -1,8 +1,13 @@
 package eu.kinstaitis.registration.test;
 
+import eu.kinstaitis.registration.Order;
+
 public class Testas {
 
 	public static void main(String[] args) {
 		System.out.println("Testing");
+
+		Order firstOrder = new Order();
+		firstOrder.setCustomer("Lina Kokyte");
 	}
 }
