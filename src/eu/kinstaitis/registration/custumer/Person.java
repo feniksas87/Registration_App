bcb23c6aca462d0c52521e93e01fdc59a3b43030
MariusKin <marius.kinstaitis@gmail.com>
2019-04-02 15:53:24 +0300

    Custumer

--- src/eu/kinstaitis/registration/custumer/Person.java
+++ src/eu/kinstaitis/registration/custumer/Person.java
@@ -17,33 +17,5 @@ public class Person {
         this.city = city;
         this.country = country;
     }
-    public boolean isNotNull(Object persona) {
-        if (this == persona) {
-            return true;
-        }
-        if (persona == null || getClass() != persona.getClass()) {
-            return false;
-        }
-        Person person = (Person) persona;
-        if (name != null ? !name.equals(person.name) : person.name != null) {
-            return false;
-        }
-        if (surname != null ? !surname.equals(person.surname) : person.surname != null) {
-            return false;
-        }
-        if (email != null ? !city.equals(person.email) : person.email != null) {
-            return false;
-        }
-        if (phoneNumber != null ? !phoneNumber.equals(person.phoneNumber) : person.phoneNumber != null) {
-            return false;
-        }
-        if (city != null ? !city.equals(person.city) : person.city != null) {
-            return false;
-        }
-        if (country != null ? !city.equals(person.country) : person.country != null) {
-            return false;
-        }
-        return true;
-    }
-
+    
 }
