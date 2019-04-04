c907734bda51cae43e08ff2f66a4f49fb5a3bdb8
MariusKin <marius.kinstaitis@gmail.com>
2019-04-04 15:51:21 +0300

    eur simbol

--- src/eu/kinstaitis/registration/order/OrderCalculator.java
+++ src/eu/kinstaitis/registration/order/OrderCalculator.java
@@ -39,7 +39,8 @@ public class OrderCalculator {
 		OrderCalculator cal = new OrderCalculator();
 
 		System.out.println();
-		System.out.println("Kaina: " + cal.calcPrice(EventType.Commercial));
+		System.out.println("Price: " + cal.calcPrice(EventType.Wedding) + "\u20ac");
+		System.out.println("Price: " + cal.calcPrice(EventType.Event) + "\u20ac");
 
 	}
 
